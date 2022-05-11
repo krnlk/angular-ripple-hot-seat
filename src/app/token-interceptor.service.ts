67b626f63070cf6ddb26d981657e51158546e302
krnlk <95872735+krnlk@users.noreply.github.com>
2022-05-11 12:46:12 +0200

    I think I finally fixed authorization.

--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-  private readonly token: string;
+  //private readonly token: string;
+  private token: string;
 
   constructor() {
       this.token = localStorage.getItem('token')||'{}'; //fix z https://www.infinetsoft.com/Post/Argument-of-type-string-null-is-not-assignable-to-parameter-of-type-string-n-Type-null-is-not-assignable-to-type-string/3092?msclkid=43a67405d08511ec83e96f00fefd8ded#.YnqgI4zP1aQ
@@ -16,7 +17,9 @@ export class TokenInterceptorService implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      if (this.token) {
+     this.token = localStorage.getItem('token')||'{}'; //fix z https://www.infinetsoft.com/Post/Argument-of-type-string-null-is-not-assignable-to-parameter-of-type-string-n-Type-null-is-not-assignable-to-type-string/3092?msclkid=43a67405d08511ec83e96f00fefd8ded#.YnqgI4zP1aQ
+
+      if (this.token != '{}') {
           const modReq = req.clone({
               setHeaders: {
                   'Authorization': "Bearer " + this.token
