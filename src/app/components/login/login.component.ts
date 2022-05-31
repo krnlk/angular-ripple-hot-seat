6d4dd895db372a0a9d7d47ec4fcdd2552782cba4
krnlk <95872735+krnlk@users.noreply.github.com>
2022-05-31 12:51:52 +0200

    Aaa

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,18 +13,12 @@ import { AppComponent } from 'src/app/app.component';
   styleUrls: ['./login.component.css']
 })
 //implements onInit
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   username!: string;
   password!: string;
-  message:any
+  message: any
 
-
-  //constructor(private service:RestapiService,private router:Router) { }
-  constructor(private http: LoginService, private router: Router) {}
-  //constructor(private http: HttpClient){
-
-
-
+  constructor(private http: LoginService, private router: Router) { }
 
   ngOnInit(): void {
 
@@ -34,46 +28,39 @@ export class LoginComponent implements OnInit{
 
   //1. przy logowaniu: albo dostajemy jsona is admin, login, password
   //2. do serwera zapytanie o usera (isAdmin, email etc)
-  doLogin(){
+  doLogin() {
     //this.http.get<any>(`http://localhost:8080/login?username=${this.username}&password=${this.password}`)
-      this.http.getLogin(this.username, this.password).subscribe(
-        response=> {
-          console.log('Response: ');
-          console.log(response);
+    this.http.getLogin(this.username, this.password).subscribe(
+      response => {
+        console.log('Response: ');
+        console.log(response);
 
-          //przetrzymywanie JWT
-          localStorage.setItem('token', response);
+        //przetrzymywanie JWT
+        localStorage.setItem('token', response);
 
-          //user info
-          localStorage.setItem('username', this.username);
-          //localStorage.setItem('isAdmin', response);
+        //user info
+        localStorage.setItem('username', this.username);
+        //localStorage.setItem('isAdmin', response);
 
 
 
-          //po udanym zalogowaniu przenosi na strone glowna
-          this.router.navigateByUrl('');
-          this.showMatToolbar();
-        },
-        error => {
-          console.log('Error: ');
-          console.log(error);
-        }
-      )
+        //po udanym zalogowaniu przenosi na strone glowna
+        this.router.navigateByUrl('');
+        this.showMatToolbar();
+      },
+      error => {
+        console.log('Error: ');
+        console.log(error);
+      }
+    )
   }
 
-  showMatToolbar(){
-    let matToolbar:any = <any>document.getElementById("matToolbar");
+  showMatToolbar() {
+    let matToolbar: any = <any>document.getElementById("matToolbar");
     matToolbar.classList.remove("hidden");
   }
 
-  /*
-  doLogin(){
-    console.log("Button is working");
-    this.service.getLogin(this.username, this.password);
-    }
-  */
-
-  }
+}
 
 
 
